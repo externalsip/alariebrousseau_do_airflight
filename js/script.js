954c0abbdc088fe578013d28b8f248910ecd6fd8
externalsip <93773873+externalsip@users.noreply.github.com>
2023-03-11 18:13:33 -0500

    swiper fuckings sucks lol

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("hi");
+
+  console.log("hi");
+const swiper_screenshots = new Swiper('.swiper-screenshots', {
+    // Optional parameters
+    direction: 'horizontal',
+
+    // If we need pagination
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView:3,
+    spaceBetween: 50,
+    loop: true,
+    centeredSlides: true,  
+  });
