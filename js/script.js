4985a97b808dba7f4627aee8f8d0c866890e0e4d
joeballls <74332549+joeballls@users.noreply.github.com>
2023-03-14 10:54:21 -0400

    fixed margin and padding
    lol

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
+console.log("hi");
+const swiper_screenshots = new Swiper(".swiper-screenshots", {
+  // Optional parameters
+  direction: "horizontal",
 
-  console.log("hi");
-const swiper_screenshots = new Swiper('.swiper-screenshots', {
-    // Optional parameters
-    direction: 'horizontal',
+  // If we need pagination
 
-    // If we need pagination
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 1,
+  spaceBetween: 50,
+  loop: true,
+  centeredSlides: true,
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
     },
-    slidesPerView:1,
-    spaceBetween: 50,
-    loop: true,
-    centeredSlides: true,
-    breakpoints: {
-        1200: {
-            slidesPerView:3,
-        }
-    }
-  });
+  },
+});
+
+var swiper_filters = new Swiper(".swiper-filter", {
+  direction: "horizontal",
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
